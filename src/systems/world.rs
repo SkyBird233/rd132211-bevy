51f3e8ab919a385df5f331ea99b8fefe597d28a8
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-18 18:03:59 +0800

    refactor: replace direct collider spawning with BlockBundle struct

--- src/systems/world.rs
+++ src/systems/world.rs
@@ -1,14 +1,37 @@
 use crate::prelude::*;
 
+#[derive(Component)]
+pub struct Block;
+
+#[derive(Bundle)]
+struct BlockBundle {
+    mark: Block,
+    collider: Collider,
+    transform: Transform,
+}
+
+impl Default for BlockBundle {
+    fn default() -> Self {
+        Self {
+            mark: Block,
+            collider: Collider::cuboid(0.5, 0.5, 0.5),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        }
+    }
+}
+
 pub fn setup_world(mut commands: Commands) {
     for x in -5..=5 {
         for z in -5..=5 {
-            commands
-                .spawn(Collider::cuboid(0.5, 0.5, 0.5))
-                .insert(Transform::from_xyz(x as f32, 0.0, z as f32));
+            commands.spawn(BlockBundle {
+                transform: Transform::from_xyz(x as f32, 0.0, z as f32),
+                ..default()
+            });
         }
     }
-    commands
-        .spawn(Collider::cuboid(0.5, 0.5, 0.5))
-        .insert(Transform::from_xyz(1.0, 2.0, 3.0));
+
+    commands.spawn(BlockBundle {
+        transform: Transform::from_xyz(1.0, 2.0, 3.0),
+        ..default()
+    });
 }
