b2386ee7f33b8d00baeae65e2a18fb2930283bea
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-19 04:02:49 +0800

    fix: .meta files 404 error on browser

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod plugins;
 pub mod prelude;
 mod systems;
 
+use bevy::asset::AssetMetaCheck;
+
 use crate::plugins::focus_plugin::FocusPlugin;
 use crate::plugins::player_plugin::PlayerPlugin;
 use crate::plugins::world_plugin::WorldPlugin;
@@ -12,14 +14,22 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-            .insert_resource(ClearColor(Color::srgb(
-                0x66 as f32 / 0xFF as f32,
-                0xCC as f32 / 0xFF as f32,
-                1.0,
-            )))
-            .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-            .add_plugins(RapierDebugRenderPlugin::default())
-            .add_plugins((WorldPlugin, PlayerPlugin, FocusPlugin));
+        app.add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                // Note: https://github.com/bevyengine/bevy/issues/10157
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                }),
+        )
+        .insert_resource(ClearColor(Color::srgb(
+            0x66 as f32 / 0xFF as f32,
+            0xCC as f32 / 0xFF as f32,
+            1.0,
+        )))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins((WorldPlugin, PlayerPlugin, FocusPlugin));
     }
 }
