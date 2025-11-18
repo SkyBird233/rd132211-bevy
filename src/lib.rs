60cb01e2efbe08e6c89897b736b228c505edaea0
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-19 02:01:21 +0800

    feat: add background color for the camera

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ mod plugins;
 pub mod prelude;
 mod systems;
 
+use crate::plugins::focus_plugin::FocusPlugin;
 use crate::plugins::player_plugin::PlayerPlugin;
 use crate::plugins::world_plugin::WorldPlugin;
-use crate::plugins::focus_plugin::FocusPlugin;
 use crate::prelude::*;
 
 pub struct GamePlugin;
@@ -13,6 +13,11 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+            .insert_resource(ClearColor(Color::srgb(
+                0x66 as f32 / 0xFF as f32,
+                0xCC as f32 / 0xFF as f32,
+                1.0,
+            )))
             .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
             .add_plugins(RapierDebugRenderPlugin::default())
             .add_plugins((WorldPlugin, PlayerPlugin, FocusPlugin));
