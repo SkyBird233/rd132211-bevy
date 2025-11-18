4f9981d7d462a82fc92ba9cffd5d70c7e7881a9d
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-11-18 17:52:14 +0800

    refactor: remove debug camera setup from world system

--- src/systems/world.rs
+++ src/systems/world.rs
@@ -1,13 +1,5 @@
 use crate::prelude::*;
 
-pub fn setup_debug_camera(mut commands: Commands) {
-    // Add a camera so we can see the debug-render.
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(10.0, 3.0, 8.0).looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
-    ));
-}
-
 pub fn setup_world(mut commands: Commands) {
     for x in -5..=5 {
         for z in -5..=5 {
